407cb09fa46ae448874687291afdefd482c36f3a
Soumyabrata Sen <soumyabratasen@Soumyabratas-MacBook-Air.local>
2026-03-12 22:10:28 +0530

    MUI-Nav APPBar modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,51 @@
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import * as Icons from "@mui/icons-material";
 
 export const NavBar = () => {
-    return (
-        <>
-        <nav className="navbar">
-            <div className="navbar-brand">
-                <Link to="/">Movie App</Link>
-            </div>
-            <div className="navbar-links">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/favorites" className="nav-link">Favorites</Link>
-                <Link to="/docs" className="nav-link">Api-docs</Link>
-            </div>
-        </nav>
-        </>
-    );
+
+  return (
+    <AppBar position="static">
+      <Toolbar className="navbar">
+        {/* Brand */}
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          className="navbar-brand"
+        >
+          Movie App
+        </Typography>
+
+        {/* Navigation buttons */}
+        <Button
+          color="inherit"
+          component={Link}
+          to="/"
+          startIcon={<Icons.Home color="success" fontSize="large" />}
+        >
+          <span style={{ color: "wheat" }}>Home</span>
+        </Button>
+
+        <Button
+          color="inherit"
+          component={Link}
+          to="/favorites"
+          startIcon={<Icons.Favorite color="secondary" fontSize="large" />}
+        >
+          <span style={{ color: "wheat" }}>Favorites</span>
+        </Button>
+
+        <Button
+          color="inherit"
+          component={Link}
+          to="/docs"
+          startIcon={<Icons.Api color="warning" fontSize="large" />}
+        >
+            <span style={{color: "wheat"}}>api-docs</span>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
